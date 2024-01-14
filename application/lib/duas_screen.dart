6665879cbe76d44068f17fd42a31e4bd6528e28c
zen` <spookywitz@yahoo.com>
2024-01-14 18:40:04 +0800

    update

---
+++ application/lib/duas_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class DuasScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Duas"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text("Duas Content"),
+      ),
+    );
+  }
+}
