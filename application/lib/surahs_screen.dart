2042a65e737d36e0e6a7e2ce4825267ff6469b65
nurulsyahina <72722275+nurulsyahina@users.noreply.github.com>
2024-01-17 23:03:58 +0800

    Update surahs_screen.dart

--- application/lib/surahs_screen.dart
+++ application/lib/surahs_screen.dart
@@ -1,17 +1,52 @@
+// surahs_screen.dart
+
 import 'package:flutter/material.dart';
+import 'package:profile_page_2/SurahALWaaqia.dart';
+import 'package:profile_page_2/SurahAlMulkPage.dart';
+import 'package:profile_page_2/SurahAnNaasPage%20copy.dart';
 
 class SurahsScreen extends StatelessWidget {
-  const SurahsScreen({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Surahs"),
-        centerTitle: true,
+        title: Text('Surah'),
       ),
-      body: const Center(
-        child: Text("Surahs Content"),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SurahAnNaasPage()),
+                );
+              },
+              child: Text('Surah An-Naas'),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SurahAlMulkPage()),
+                );
+              },
+              child: Text('Surah Al-Mulk'),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SurahAlWaaqiaPage()),
+                );
+              },
+              child: Text('Surah Al-Waaqia'),
+            ),
+          ],
+        ),
       ),
     );
   }
